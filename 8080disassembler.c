63098be6bd049610c5efa868ef0e9123bc13c644
italo <irs@cin.ufpe.br>
2019-10-11 22:52:26 -0300

    Second group of instructions

---
+++ 8080disassembler.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Recebe o código todo num buffer e inicializa o PC
+int Disassemble8080(unsigned char *codebuffer, int pc){
+    unsigned char *code = &codebuffer[pc];
+    int opbytes=1;
+    //Guarda os bytes do OPCODE
+    //Imprime o PC com 4 zeros na frente e formata como hexadecimal
+    printf("%04x\n", pc);
+    switch (*code)
+    {
+        case 0x00: printf("NOP"); break;
+		case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes=3; break;
+		case 0x02: printf("STAX   B"); break;
+		case 0x03: printf("INX    B"); break;
+		case 0x04: printf("INR    B"); break;
+		case 0x05: printf("DCR    B"); break;
+		case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;
+		case 0x07: printf("RLC"); break;
+		case 0x08: printf("NOP"); break;
+		case 0x09: printf("DAD    B"); break;
+		case 0x0a: printf("LDAX   B"); break;
+		case 0x0b: printf("DCX    B"); break;
+		case 0x0c: printf("INR    C"); break;
+		case 0x0d: printf("DCR    C"); break;
+		case 0x0e: printf("MVI    C,#$%02x", code[1]); opbytes = 2;	break;
+		case 0x0f: printf("RRC"); break;
+			
+		case 0x10: printf("NOP"); break;
+		case 0x11: printf("LXI    D,#$%02x%02x", code[2], code[1]); opbytes=3; break;
+		case 0x12: printf("STAX   D"); break;
+		case 0x13: printf("INX    D"); break;
+		case 0x14: printf("INR    D"); break;
+		case 0x15: printf("DCR    D"); break;
+		case 0x16: printf("MVI    D,#$%02x", code[1]); opbytes=2; break;
+		case 0x17: printf("RAL"); break;
+		case 0x18: printf("NOP"); break;
+		case 0x19: printf("DAD    D"); break;
+		case 0x1a: printf("LDAX   D"); break;
+		case 0x1b: printf("DCX    D"); break;
+		case 0x1c: printf("INR    E"); break;
+		case 0x1d: printf("DCR    E"); break;
+		case 0x1e: printf("MVI    E,#$%02x", code[1]); opbytes = 2; break;
+		case 0x1f: printf("RAR"); break;
+			
+		
+    }
+    return opbytes;
+}
+
+int main (int argc, char**argv)    
+   {    
+    FILE *f= fopen(argv[1], "rb");    
+    if (f==NULL)    
+    {    
+        printf("error: Couldn't open %s\n", argv[1]);    
+        exit(1);    
+    }
+
+    //Get the file size and read it into a memory buffer    
+    fseek(f, 0L, SEEK_END);    
+    int fsize = ftell(f);    
+    fseek(f, 0L, SEEK_SET);    
+
+    unsigned char *buffer=malloc(fsize);    
+
+    fread(buffer, fsize, 1, f);    
+    fclose(f);    
+
+    int pc = 0;    
+    
+    while (pc < fsize)    
+    {    
+        pc += Disassemble8080(buffer, pc);    
+    }    
+    return 0;    
+}
